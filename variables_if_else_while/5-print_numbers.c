e181d0b5dbcccdc4b19ab0e048b615cd51db1b22
lolalex1 <alex.atanasovski03@gmail.com>
2025-02-23 13:29:31 +0000

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n")
+
+	return (0);
+}
