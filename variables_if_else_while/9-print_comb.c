564eb720f5f5bced0087d972e9ad44899bc761b7
lolalex1 <alex.atanasovski03@gmail.com>
2025-02-23 13:38:22 +0000

    Tasks from 7-9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print all combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
