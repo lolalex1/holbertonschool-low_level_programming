73a43926b36a3cdc7552a3c56d0c4e73687b1ca8
lolalex1 <alex.atanasovski03@gmail.com>
2025-02-26 08:14:27 +0000

    Return description for function

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - Checking if a letter is lowercase
+ * _islower - Checking if a letter is lowercase.
  * @c: The character to check.
  *
  * Description: Return 1 if lowercase, 0 otherwise.
+ * 
+ * Return: Will return 1 if c is lowercase, otherwise will return 0.
  */
 int _islower(int c)
 {
