9e68c1461c1d17f7b45e9bfb1e5c848964d9db5a
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-04 00:36:38 +0000

    Task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
+ * _strchr - Locating a specfic character in a string
+ * @s: pointer
+ * @c: destination letter
  *
- *
+ * Return: Pointer to c Or null if not there.
  */
 char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
 		if (*s == c)
-			return s;
+			return (s);
 		s++;
 	}
 
