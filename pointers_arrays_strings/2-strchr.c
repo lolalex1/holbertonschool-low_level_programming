020baa94d274a1f0191dd0b1953ef4e639ca873e
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-04 00:34:17 +0000

    Task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+
+	if (c == '\0')
+		return (s);
+
+	return (0);
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
