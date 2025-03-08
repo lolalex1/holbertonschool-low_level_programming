18c8b28744c373fc5152e207a18ed6e75a13b7dd
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-08 06:11:57 +0000

    Indentation to tabs

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
-        if (needle[j] == '\0')
-            return (haystack + i);
-    }
-    return (0);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (0);
 }
