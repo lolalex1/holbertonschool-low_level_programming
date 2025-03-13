dceb8d34cb53f42f5a110c8591137b2d246ae0c5
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-13 00:27:44 +0000

    Slight rewrite

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,13 +15,17 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
+	char *arr;
+
 	char *arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL) {
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++) {
+	unsigned int i;
+
+	for (i = 0; i < size; i++) {
 		arr[i] = c;
 	}
 
