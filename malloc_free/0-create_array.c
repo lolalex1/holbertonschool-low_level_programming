48b18aaf5572144ee7c3bae0c1d79c9658aba03d
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-13 00:29:21 +0000

    More

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,19 +11,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0) {
 		return (NULL);
 	}
 
-	char *arr;
-
-	char *arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL) {
 		return (NULL);
 	}
-
-	unsigned int i;
 
 	for (i = 0; i < size; i++) {
 		arr[i] = c;
