a9874b134db2ddc6524b9f33db409e84a9299226
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-13 00:31:19 +0000

    Betty coding

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,17 +14,20 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size == 0) {
+	if (size == 0)
+	{
 		return (NULL);
 	}
 
 	arr = (char *)malloc(sizeof(char) * size);
 
-	if (arr == NULL) {
+	if (arr == NULL)
+	{
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
 		arr[i] = c;
 	}
 
