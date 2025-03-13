131b13515d24bac9dd7619697aaa8cfcfcdafc10
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-13 00:24:26 +0000

    First task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create-array - Array of chars initalized specfic char
+ *
+ * @size: size of memory to print
+ * @c: Inizilaising character
+ *
+ * Return: Pointer to array, null otherwise
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0) {
+		return (NULL);
+	}
+
+	char *arr = (char *)malloc(sizeof(char) * size);
+
+	if (arr == NULL) {
+		return (NULL);
+	}
+
+	for (unsigned int i = 0; i < size; i++) {
+		arr[i] = c;
+	}
+
+	return (arr);
+}
+
---
+++ malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
