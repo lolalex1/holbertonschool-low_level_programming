d62957ec9bd3aed3413ae6b4ef1d2973ac986b0d
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-22 09:12:39 +0000

    Curious if this passes betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -16,7 +16,10 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		if (format[i] == 'c' ||
+		    format[i] == 'i' ||
+		    format[i] == 'f' ||
+		    format[i] == 's')
 		{
 			printf("%s", sep);
 			switch (format[i])
